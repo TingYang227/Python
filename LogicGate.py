10863fb99fc362548691228a1487f628c4f08487
Ting <39301486+TingYang227@users.noreply.github.com>
2019-02-05 16:45:16 +0000

    LogicGate
    LogicGate

--- LogicGate.py
+++ LogicGate.py
@@ -37,4 +37,51 @@ class UnaryGate(LogicGate):
         
     def getPin(self):
         return int(input("Enter Pin input for gate " + self.getLabel() + "-->"))
+
+
+class AndGate(BinaryGate):
+    
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
         
+    def performGateLogic(self):
+        
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+# Test
+# g1 = AndGate("G1")
+# g1.getOutput()
+        
+
+class OrGate(BinaryGate):
+    
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+        
+    def performGateLogic(self):
+        
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 0 and b == 0:
+            return 0
+        else:
+            return 1
+
+
+class NotGate(UnaryGate):
+    
+    def __init__(self, n):
+        UnaryGate.__init__(self, n)
+        
+    def performGateLogic(self):
+        
+        a = getPin()
+        if a == 1:
+            return 0
+        else:
+            return 1

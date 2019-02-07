ca21b8092a6cc0d97be45aa49537774f1c50ff2c
Ting <39301486+TingYang227@users.noreply.github.com>
2019-02-07 17:27:44 +0000

    Stack
    Stack

--- BalancedSymbols.py
+++ BalancedSymbols.py
@@ -1,24 +1,5 @@
 # A general case
-
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return self.items == []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def peek(self):
-        return self.items[len(self.items)-1]
-
-    def size(self):
-        return len(self.items)
-
+from Stack import Stack
 
 def parChecker(symbolString):
     s = Stack()
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ def main():
     print("Import Stack successfully")
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()

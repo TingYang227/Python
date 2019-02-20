567f1de1dc5e96a32e86c39a21d95b9ef2996f03
Ting <39301486+TingYang227@users.noreply.github.com>
2019-02-20 14:49:14 +0000

    Maze
    Maze

--- Maze.py
+++ Maze.py
@@ -36,4 +36,42 @@ class Maze:
 
     def drawMaze(self):
         self.t.speed(10)
+        self.wn.tracer(0)
+        for y in range(self.rowsInMaze):
+            for x in range(self.columnsInMaze):
+                if self.mazelist[y][x] == OBSTACLE:
+                    self.drawCenteredBox(x+self.xTranslate,-y+self.yTranslate,'orange')
+        self.t.color('black')
+        self.t.fillcolor('blue')
+        self.wn.update()
+        self.wn.tracer(1)
+
+    def drawCenteredBox(self,x,y,color):
+        self.t.up()
+        self.t.goto(x-.5,y-.5)
+        self.t.color(color)
+        self.t.fillcolor(color)
+        self.t.setheading(90)
+        self.t.down()
+        self.t.begin_fill()
+        for i in range(4):
+            self.t.forward(1)
+            self.t.right(90)
+        self.t.end_fill()
+
+    def moveTurtle(self, x, y):
+        self.t.up()
+        self.t.setheading(self.t.towards(x+self.xTranslate, -y+self.yTranslate))
+        self.t.goto(x+self.xTranslate, -y+self.yTranslate)
+
+    def dropBreadcrumb(self, color):
+        self.t.dot(10, color)
+
+    def updatePosition(self, row, col, val=None):
+        if val:
+            self.mazelist[row][col] = val
+        self.moveTurtle(col, row)
+
+        
+
 

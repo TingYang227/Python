f585ff5fd2a7589171f703119cde376054361b01
Ting <39301486+TingYang227@users.noreply.github.com>
2019-03-25 16:39:26 +0000

    pd.Series
    pd.Series

--- MatPlot.py
+++ MatPlot.py
@@ -2,10 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-height = ['170','179','159','160','180','164','168','174','160','183']
-weight = ['57','62','47','67','59','49','54','63','66','80']
+height = np.sort(['170','179','159','160','180','164','168','174','160','183'])
+weight = np.sort(['57','62','47','67','59','49','54','63','66','80'])
 
-plt.scatter(height, weight, s=40, c='b', marker='.')
+print(len(height))
+print(len(weight))
+
+plt.scatter(height, weight, s=40, c='r', marker='.')
 plt.xlabel('height(cm)')
 plt.ylabel('weight(kg)')
 plt.title('Test')
